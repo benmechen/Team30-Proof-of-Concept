c67aa284eca91accc6f57bdce6232375ef8e9ca6
Ben Mechen <psybm7@nottingham.ac.uk>
2019-12-02 14:11:41 +0000

    First commit

--- InputTest/Test/BluetoothViewController.swift
+++ InputTest/Test/BluetoothViewController.swift
@@ -134,12 +134,6 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
         */
         func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
             
-            // Reject any where the value is above reasonable range
-            // Reject if the signal strength is too low to be close enough (Close is around -22dB)
-    //        if  RSSI.integerValue < -15 && RSSI.integerValue > -35 {
-    //            println("Device not at correct range")
-    //            return
-    //        }
             print("Discovered \(peripheral.name) at \(RSSI)")
             devices[RSSI.intValue] = peripheral
             refresh()
@@ -157,16 +151,12 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
             }
         }
         
-        /** If the connection fails for whatever reason, we need to deal with it.
-        */
         func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
             print("Failed to connect to \(peripheral). (\(error!.localizedDescription))")
             
             cleanup()
         }
 
-        /** We've connected to the peripheral, now we need to discover the services and characteristics to find the 'transfer' characteristic.
-        */
         func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
             print("Peripheral Connected")
             
@@ -185,8 +175,6 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
 //            peripheral.discoverServices([transferServiceUUID])
         }
         
-        /** The Transfer Service was discovered
-        */
         func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
             guard error == nil else {
                 print("Error discovering services: \(error!.localizedDescription)")
@@ -198,19 +186,12 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
                 return
             }
 
-            // Discover the characteristic we want...
-            
-            // Loop through the newly filled peripheral.services array, just in case there's more than one.
             for service in services {
-                peripheral.discoverCharacteristics([transferCharacteristicUUID], for: service)
+                peripheral.discoverCharacteristics([], for: service)
             }
         }
         
-        /** The Transfer characteristic was discovered.
-        *  Once this has been found, we want to subscribe to it, which lets the peripheral know we want the data it contains
-        */
         func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-            // Deal with errors (if any)
             guard error == nil else {
                 print("Error discovering services: \(error!.localizedDescription)")
                 cleanup()
@@ -222,19 +203,13 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
                 return
             }
 
-            // Again, we loop through the array, just in case.
             for characteristic in characteristics {
-                // And check if it's the right one
                 if characteristic.uuid.isEqual(transferCharacteristicUUID) {
-                    // If it is, subscribe to it
                     peripheral.setNotifyValue(true, for: characteristic)
                 }
             }
-            // Once this is complete, we just need to wait for the data to come in.
         }
         
-        /** This callback lets us know more data has arrived via notification on the characteristic
-        */
         func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
             guard error == nil else {
                 print("Error discovering services: \(error!.localizedDescription)")
@@ -246,34 +221,24 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
                 return
             }
 
-            // Have we got everything we need?
             if stringFromData.isEqual(to: "EOM") {
-                // We have, so show the data,
 //                textView.text = String(data: data.copy() as! Data, encoding: String.Encoding.utf8)
 
-                // Cancel our subscription to the characteristic
+                // Cancel  subscription to the characteristic
                 peripheral.setNotifyValue(false, for: characteristic)
 
-                // and disconnect from the peripehral
+                // Disconnect from the peripehral
                 centralManager?.cancelPeripheralConnection(peripheral)
             } else {
-                // Otherwise, just add the data on to what we already have
                 data.append(characteristic.value!)
 
-                // Log it
                 print("Received: \(stringFromData)")
             }
         }
 
-        /** The peripheral letting us know whether our subscribe/unsubscribe happened or not
-        */
         func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
             print("Error changing notification state: \(error?.localizedDescription)")
             
-            // Exit if it's not the transfer characteristic
-            guard characteristic.uuid.isEqual(transferCharacteristicUUID) else {
-                return
-            }
             
             // Notification has started
             if (characteristic.isNotifying) {
@@ -284,28 +249,19 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
             }
         }
         
-        /** Once the disconnection happens, we need to clean up our local copy of the peripheral
-        */
         func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
             print("Peripheral Disconnected")
             discoveredPeripheral = nil
             
-            // We're disconnected, so start scanning again
+            // Disconnected, start scanning again
             scan()
         }
         
-        /** Call this when things either go wrong, or you're done with the connection.
-        *  This cancels any subscriptions if there are any, or straight disconnects if not.
-        *  (didUpdateNotificationStateForCharacteristic will cancel the connection if a subscription is involved)
-        */
         fileprivate func cleanup() {
-            // Don't do anything if we're not connected
-            // self.discoveredPeripheral.isConnected is deprecated
             guard discoveredPeripheral?.state == .connected else {
                 return
             }
             
-            // See if we are subscribed to a characteristic on the peripheral
             guard let services = discoveredPeripheral?.services else {
                 cancelPeripheralConnection()
                 return
@@ -319,7 +275,6 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
                 for characteristic in characteristics {
                     if characteristic.uuid.isEqual(transferCharacteristicUUID) && characteristic.isNotifying {
                         discoveredPeripheral?.setNotifyValue(false, for: characteristic)
-                        // And we're done.
                         return
                     }
                 }
@@ -327,7 +282,6 @@ extension BluetoothViewController: CBPeripheralDelegate, CBCentralManagerDelegat
         }
 
         fileprivate func cancelPeripheralConnection() {
-            // If we've got this far, we're connected, but we're not subscribed, so we just disconnect
             centralManager?.cancelPeripheralConnection(discoveredPeripheral!)
         }
 }
